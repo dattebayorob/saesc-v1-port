619bb0b888c545f3ff39c4fd6e4332252549741b
= <=>
2019-01-25 16:57:53 -0300

    Controller para conversao. TODO: Persistencia dos links errada, errei no relacionamento da Entidade Link, necessário correção

---
+++ src/main/java/com/dtb/saescapiold/controller/ConverterController.java
@@ -0,0 +1,41 @@
+package com.dtb.saescapiold.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.dtb.saescapiold.model.converters.ConverterEntidades;
+import com.dtb.saescapiold.model.entities.Escola;
+import com.dtb.saescapiold.service.EscolaIpService;
+import com.dtb.saescapiold.service.EscolaService;
+import com.dtb.saescapiold.service.EscolaV2Service;
+import com.dtb.saescapiold.service.LinkService;
+
+@RestController
+@RequestMapping(value = "/converter")
+public class ConverterController {
+	@Autowired
+	private EscolaService escolaService;
+	@Autowired
+	private EscolaV2Service v2Service;
+	@Autowired
+	private LinkService linkService;
+	@Autowired
+	private EscolaIpService ipService;
+	private static ConverterEntidades converter = new ConverterEntidades();
+
+	@GetMapping("/todos")
+	public ResponseEntity<String> converterTodos() {
+		List<Escola> escolas = escolaService.buscarTodas();
+		escolas.forEach(escola -> {
+			v2Service.adicionar(converter.toV2(escola));
+			//converter.converterLinks(ipService.buscarPelaEscola(escola.getId()))
+				//	.forEach(link -> linkService.adicionar(link));
+		});
+		return ResponseEntity.ok().build();
+	}
+}
