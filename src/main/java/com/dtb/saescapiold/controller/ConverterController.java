38002c250d03e44e38c4ea0c0b44bd436bd2a820
dattebayoRob <robson.william65@gmail.com>
2019-03-20 13:53:18 -0300

    Log endpoint

--- src/main/java/com/dtb/saescapiold/controller/ConverterController.java
+++ src/main/java/com/dtb/saescapiold/controller/ConverterController.java
@@ -2,6 +2,8 @@ package com.dtb.saescapiold.controller;
 
 import java.util.List;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -28,15 +30,23 @@ public class ConverterController {
 	@Autowired
 	private EscolaIpService ipService;
 	private static ConverterEntidades converter = new ConverterEntidades();
+	private static final Logger log = LoggerFactory.getLogger(ConverterController.class);
 
 	@GetMapping("/todos")
 	public ResponseEntity<String> converterTodos() {
 		List<Escola> escolas = escolaService.buscarTodas();
 		escolas.forEach(escola -> {
+			log.info("Convertendo escola: {}", escola.toString());
 			EscolaV2 v2 = v2Service.adicionar(converter.toV2(escola));
-			converter.converterLinks(v2.getId(),ipService.buscarPelaEscola(escola.getId()))
-					.forEach(link -> linkService.adicionar(link));
+			log.info("Nova entidade escola gerada de Id: {}", escola.getId());
+			converter.converterLinks(v2.getId(), ipService.buscarPelaEscola(escola.getId()))
+					.forEach(link -> {
+						linkService.adicionar(link);
+						log.info("Link adicionado para a escola :{}",escola.getNome());
+						});
 		});
-		return ResponseEntity.ok().body(escolas.size()+" Convertidas");
+		String msg = escolas.size() + " Convertidas";
+		log.info(msg);
+		return ResponseEntity.ok().body(msg);
 	}
 }
