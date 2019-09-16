54cfcd83bc605e9e4e9c1ffae2b191ee440ac3bb
dattebayorob <robson.william65@gmail.com>
2019-09-15 21:58:26 -0300

    Converte entities on service

--- src/main/java/com/dtb/saescapiold/service/EscolaService.java
+++ src/main/java/com/dtb/saescapiold/service/EscolaService.java
@@ -1,9 +1,5 @@
 package com.dtb.saescapiold.service;
 
-import java.util.List;
-
-import com.dtb.saescapiold.model.entities.Escola;
-
 public interface EscolaService {
-	List<Escola> buscarTodas();
+	Long converterTodas();
 }
--- src/main/java/com/dtb/saescapiold/service/impl/EscolaServiceImpl.java
+++ src/main/java/com/dtb/saescapiold/service/impl/EscolaServiceImpl.java
@@ -1,21 +1,43 @@
 package com.dtb.saescapiold.service.impl;
 
-import java.util.List;
-
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import com.dtb.saescapiold.model.entities.Escola;
+import com.dtb.saescapiold.model.converters.ConverterEntidades;
+import com.dtb.saescapiold.model.entities.EscolaV2;
 import com.dtb.saescapiold.model.repositories.EscolaRepository;
+import com.dtb.saescapiold.service.EscolaIpService;
 import com.dtb.saescapiold.service.EscolaService;
+import com.dtb.saescapiold.service.EscolaV2Service;
+import com.dtb.saescapiold.service.LinkService;
+
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 
 @Service
+@Slf4j
+@AllArgsConstructor
 public class EscolaServiceImpl implements EscolaService{
-	@Autowired
-	private EscolaRepository escolaRepository;
-
+	private final EscolaRepository escolaRepository;
+	private final EscolaV2Service v2Service;
+	private final LinkService linkService;
+	private final EscolaIpService ipService;
+	private final ConverterEntidades converter;
+	
 	@Override
-	public List<Escola> buscarTodas() {
-		return escolaRepository.findAll();
+	public Long converterTodas() {
+		return escolaRepository
+				.findAll()
+				.stream()
+				.peek(escola -> {
+					log.info("Convertendo escola: {}", escola);
+					EscolaV2 v2 = v2Service.adicionar(converter.toV2(escola));
+					log.info("Nova entidade escola gerada de Id: {}", escola.getId());
+					converter.converterLinks(v2.getId(), ipService.buscarPelaEscola(escola.getId()))
+							.forEach(link -> {
+								linkService.adicionar(link);
+								log.info("Link adicionado para a escola :{}",escola.getNome());
+								});
+				})
+				.count();
 	}
 }
